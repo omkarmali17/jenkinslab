347f9310a0ee58c3603fd9002823db52adc25d88
omkarmali17 <omkarmali51@gmail.com>
2022-10-30 12:12:10 +0530

    final

--- helloworld.java
+++ helloworld.java
@@ -4,7 +4,8 @@ public class helloworld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello world...! & Hello Jenkins...!");
+		System.out.println("Hello world...!");
+		System.out.println("Hello Jenkins...!");
 	}
 
 }
